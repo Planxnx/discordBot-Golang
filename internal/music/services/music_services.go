25b5b422eb94ee006c88d3ed226eeba3ff749971
Thanee Charattrakool <planxthanee@gmail.com>
2020-07-06 04:07:44 +0700

    [Added]: music model

--- internal/music/services/music_services.go
+++ internal/music/services/music_services.go
@@ -4,25 +4,34 @@ import (
 	"context"
 	"errors"
 
+	"github.com/Planxnx/discordBot-Golang/internal/music/model"
 	"github.com/rylio/ytdl"
 )
 
 //GetYoutubeDownloadURL return youtube download url
-func GetYoutubeDownloadURL(link string) (string, error) {
+func GetYoutubeDownloadURL(link string) (*model.Song, error) {
 	ctx := context.Background()
 	client := ytdl.DefaultClient
 	videoInfo, err := client.GetVideoInfo(ctx, link)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
+
 	for _, format := range videoInfo.Formats {
 		if format.AudioEncoding == "opus" || format.AudioEncoding == "aac" || format.AudioEncoding == "vorbis" {
 			data, err := client.GetDownloadURL(ctx, videoInfo, format)
 			if err != nil {
-				return "", err
+				return nil, err
 			}
-			return data.String(), nil
+			return &model.Song{
+				Title:        videoInfo.Title,
+				Link:         link,
+				DownloadLink: data.String(),
+				Duration:     videoInfo.Duration,
+				Uploader:     videoInfo.Uploader,
+				ThumbnailURL: videoInfo.GetThumbnailURL(ytdl.ThumbnailQualityHigh),
+			}, nil
 		}
 	}
-	return "", errors.New("Audio format not found")
+	return nil, errors.New("Audio format not found")
 }
